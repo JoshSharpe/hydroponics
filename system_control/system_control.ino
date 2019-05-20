3f486d6c00929872c933d9ebcb6331eb9e734d75
Josh Sharpe <joshasharpe@gmail.com>
2019-05-20 15:01:23 -0700

    Working Timed Light Switch
    Rough code that switches lights (or switch, whichever is plugged in) on and off every 5 seconds.

--- system_control/system_control.ino
+++ system_control/system_control.ino
@@ -2,45 +2,78 @@ char currentInput;
 bool newData = false;
 bool pumpOn = false;
 
-char TURN_PUMP_ON = '1';
-char TURN_PUMP_OFF = '0';
+const char TURN_PUMP_ON = '1';
+const char TURN_PUMP_OFF = '0';
 
-int PUMP_SWITCH_PIN = 13;
+const int PUMP_SWITCH_PIN = 30;
 
-void setup() {
+unsigned long lightTriggerTime;
+
+bool isLightOn = false;
+
+void setup() 
+{
   Serial.begin(9600);
   pinMode(PUMP_SWITCH_PIN, OUTPUT);
+  digitalWrite(PUMP_SWITCH_PIN, LOW);
   Serial.println("Starting program.");
+
+  lightTriggerTime = millis();
 }
 
-void loop() {
-  checkInput();
-  if(!newData) {
-    continue;
+void loop() 
+{
+//  checkInput();
+//  if(!newData) {
+//    return;
+//  }
+//
+//  switch(currentInput) {
+//    case TURN_PUMP_ON:
+//      Serial.println("Turning pump on.");
+//      digitalWrite(PUMP_SWITCH_PIN, HIGH);
+//      break;
+//    case TURN_PUMP_OFF:
+//      Serial.println("Turning pump off.");
+//      digitalWrite(PUMP_SWITCH_PIN, LOW);
+//      break;
+//    default:
+//      Serial.println("Invalid option.");
+//      break;
+//  }
+
+  unsigned long currentTime = millis();
+
+  Serial.print("currentTime: ");
+  Serial.println(currentTime);
+  Serial.print("getSeconds(5): ");
+  Serial.println(getSeconds(5));
+  Serial.print("lightTriggerTime: ");
+  Serial.println(lightTriggerTime);
+
+  if(currentTime > getSeconds(5) && currentTime - getSeconds(5) >= lightTriggerTime) {
+    Serial.println("Triggering light.");
+    triggerLight();
+    lightTriggerTime = currentTime;
   }
 
-  switch(currentInput) {
-    case TURN_PUMP_ON:
-      Serial.Println("Turning pump on.");
-      digitalWrite(PUMP_SWITCH_PIN, HIGH);
-      break;
-    case TURN_PUMP_OFF:
-      Serial.Println("Turning pump off.");
-      digitalWrite(PUMP_SWITCH_PIN, LOW);
-      break;
-    default:
-      Serial.Println("Invalid option.");
-      break;
-  }
+  newData = false;
 }
 
 void checkInput() {
   if(Serial.available() > 0) {
     currentInput = Serial.read();
+    Serial.println("current Input: ");
+    Serial.println(currentInput);
     newData = true;
   }
 }
 
-void triggerPump() {
-  
+int getSeconds(int numberOfSeconds) {
+  return numberOfSeconds * 1000;
+}
+
+void triggerLight() {
+  isLightOn = !isLightOn;
+  digitalWrite(PUMP_SWITCH_PIN, isLightOn);
 }
